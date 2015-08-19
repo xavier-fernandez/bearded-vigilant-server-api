e2169102e6c13a2d2a7303f2abbdd607915c1e80
Xavier Fernández Salas <xavier.fernandez.salas@gmail.com>
2015-08-19 16:41:39 +0200

    ENV-68: Implement GORM framework
    - Creates the SQLite database 3 on  "databases/enviro-gatt.db"
    - Enables the log mode since the application is still on development.

--- Main.go
+++ Main.go
@@ -1,8 +1,24 @@
 package main
 
+import (
+	"fmt"
+	"github.com/jinzhu/gorm"
+	_ "github.com/mattn/go-sqlite3"
+)
+
 const APP_NAME = "enviro-vigilant"
 const APP_VERSION = "0.0.1"
 
+const DATABASE_LOCATION = "databases/enviro-gatt.db"
+
 func main() {
 	println("Loading", APP_NAME, "version", APP_VERSION)
+	db, err := gorm.Open("sqlite3", DATABASE_LOCATION)
+	if err != nil {
+		fmt.Errorf("The following error was thrown when initializing the database -> ", err)
+	}
+	// Initialize the database connection
+	db.DB()
+	// Enable database Logging.
+	db.LogMode(true)
 }
