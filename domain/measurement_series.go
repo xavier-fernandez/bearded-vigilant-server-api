700e8bd37ee02df7893e3514384abd9e2210b368
Xavier Fernández Salas <xavier.fernandez.salas@gmail.com>
2015-08-21 12:02:39 +0200

    ENV-67: Make the series StartTimestamp compulsory

--- domain/measurement_series.go
+++ domain/measurement_series.go
@@ -3,8 +3,8 @@ package domain
 import "time"
 
 type MeasurementSeries struct {
-	ID                uint32     `sql:"AUTO_INCREMENT"; json:"id"`
-	StartTimestamp    time.Time  `sql:"size:29"; json:"id"`
+	ID                uint64     `sql:"AUTO_INCREMENT"; json:"id"`
+	StartTimestamp    time.Time  `sql:"NOT NULL"; json:"id"`
 	EndTimestamp      *time.Time `sql:"size:29"; json:"endTimestamp"`
 	MeasurementEntity []Measurement
 }
