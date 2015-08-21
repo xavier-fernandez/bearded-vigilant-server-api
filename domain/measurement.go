b10b4b30ece42b4f667c99205869dcb5daca7a46
Xavier Fernández Salas <xavier.fernandez.salas@gmail.com>
2015-08-21 12:59:33 +0200

    ENV-67: Update JSON serialization domain names

--- domain/measurement.go
+++ domain/measurement.go
@@ -4,9 +4,9 @@ import "time"
 
 type Measurement struct {
 	ID             uint64     `sql:"AUTO_INCREMENT; NOT NULL"; json:"id"`
-	Value          float64    `sql:"NOT NULL";`
-	BinSize        uint16     `sql:"NOT NULL";`
-	StartTimestamp time.Time  `sql:"NOT NULL"; json:"id"`
+	Value          float64    `sql:"NOT NULL"; json:"value"`
+	BinSize        uint16     `sql:"NOT NULL"; json:"binSize"`
+	StartTimestamp time.Time  `sql:"NOT NULL"; json:"startTimestamp"`
 	EndTimestamp   *time.Time `json:"endTimestamp"`
 	Latitude       float32    `sql:"size:4"; json:"latitude"`
 	Longitude      float32    `sql:"size:4"; json:"longitude"`
--- domain/measurement_series.go
+++ domain/measurement_series.go
@@ -4,7 +4,7 @@ import "time"
 
 type MeasurementSeries struct {
 	ID                uint64     `sql:"AUTO_INCREMENT; NOT NULL"; json:"id"`
-	StartTimestamp    time.Time  `sql:"NOT NULL"; json:"id"`
+	StartTimestamp    time.Time  `sql:"NOT NULL"; json:"startTimestamp"`
 	EndTimestamp      *time.Time `json:"endTimestamp"`
 	MeasurementEntity []Measurement
 }
