64038ef8d80f6605bbb8c34cce69c277e6ee4458
Xavier Fernández Salas <xavier.fernandez.salas@gmail.com>
2015-08-21 09:40:55 +0200

    ENV-66: Move database logic to a persistence package

---
+++ persistance/database_connector.go
@@ -0,0 +1,31 @@
+package persistance
+
+import (
+	"fmt"
+	"github.com/environ-vigilant/domain"
+	"github.com/jinzhu/gorm"
+	_ "github.com/lib/pq"
+)
+
+const databaseUser = "envirodb"
+const databasePassword = "H8L@r3@{~?&~}w|J33$6!837?l"
+
+var db gorm.DB
+
+// Obtains a database connector, if available.
+func InitDatabase() error {
+	database, err := gorm.Open("postgres", "user=Xavi dbname=Xavi sslmode=disable")
+	if err != nil {
+		fmt.Errorf("The following error was thrown when initializing the database -> ", err)
+		return err
+	}
+	// Initialize the database connection
+	database.DB()
+	// Enable database Logging.
+	database.LogMode(true)
+	// Creates the database tables.
+	database.CreateTable(&domain.MeasurementSeries{})
+	database.CreateTable(&domain.Measurement{})
+	db = database
+	return nil
+}
