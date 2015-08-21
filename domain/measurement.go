d946f5a82a25a56542ad532cf73c96bf5f3e4d48
Xavier Fernández Salas <xavier.fernandez.salas@gmail.com>
2015-08-21 12:32:42 +0200

    ENV-67: Measurement stores the Value and the BinSize

--- domain/measurement.go
+++ domain/measurement.go
@@ -4,6 +4,8 @@ import "time"
 
 type Measurement struct {
 	ID             uint64     `sql:"AUTO_INCREMENT; NOT NULL"; json:"id"`
+	Value          float64    `sql:"NOT NULL";`
+	BinSize        uint16     `sql:"NOT NULL";`
 	StartTimestamp time.Time  `sql:"NOT NULL"; json:"id"`
 	EndTimestamp   *time.Time `json:"endTimestamp"`
 	Latitude       float32    `sql:"size:4"; json:"latitude"`
