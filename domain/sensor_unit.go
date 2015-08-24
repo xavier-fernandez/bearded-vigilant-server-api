cd0b902390d700038edd1d98590f110b4ef2dd91
Xavier Fernández Salas <xavier.fernandez.salas@gmail.com>
2015-08-24 12:36:08 +0200

    ENV-67: Add 'Unit' attribute to the sensor unit

--- domain/sensor_unit.go
+++ domain/sensor_unit.go
@@ -3,4 +3,5 @@ package domain
 type SensorUnit struct {
 	ID   uint64 `sql:"AUTO_INCREMENT; NOT NULL"; json:"id"`
 	Name string `sql:"UNIQUE; NOT NULL"; json:"name"`
+	Unit string `sql:"UNIQUE; NOT NULL"; json:"unit"`
 }
