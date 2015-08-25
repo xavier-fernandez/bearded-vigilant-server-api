3e84a8525f425814971f5aea5b68940fc6489dd7
Xavier Fernández Salas <xavier.fernandez.salas@gmail.com>
2015-08-25 11:31:26 +0200

    ENV-67: SensorType ID output attribute is compulsory

--- domain/sensor_type.go
+++ domain/sensor_type.go
@@ -1,12 +1,12 @@
 package domain
 
 type SensorType struct {
-	ID                 uint64 `sql:"AUTO_INCREMENT; NOT NULL"; json:"id"`
-	Name               string `sql:"UNIQUE; NOT NULL"; json:"name"`
-	IsAnalog           bool   `sql:"NOT NULL"; json:"isAnalog"`
-	IsDigital          bool   `sql:"NOT NULL"; json:"isDigital"`
-	IsPwm              bool   `sql:"NOT NULL"; json:"isPAM"`
-	IsI2c              bool   `sql:"NOT NULL"; json:"isI2C"`
-	SensorOutputTypeId uint64
+	ID                 uint64            `sql:"AUTO_INCREMENT; NOT NULL"; json:"id"`
+	Name               string            `sql:"UNIQUE; NOT NULL"; json:"name"`
+	IsAnalog           bool              `sql:"NOT NULL"; json:"isAnalog"`
+	IsDigital          bool              `sql:"NOT NULL"; json:"isDigital"`
+	IsPwm              bool              `sql:"NOT NULL"; json:"isPAM"`
+	IsI2c              bool              `sql:"NOT NULL"; json:"isI2C"`
+	SensorOutputTypeId uint64            `sql:"NOT NULL";`
 	SensorOutputType   SensorOutputType
 }
