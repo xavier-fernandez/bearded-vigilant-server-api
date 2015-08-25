0048f749f701fdeb1741117eb1242f51e32b4240
Xavier Fernández Salas <xavier.fernandez.salas@gmail.com>
2015-08-25 10:35:23 +0200

    ENV-67: Rename 'sensor_unit' to 'sensor_output_type'

--- domain/sensor_output_type.go
+++ domain/sensor_output_type.go
@@ -1,6 +1,6 @@
 package domain
 
-type SensorUnit struct {
+type SensorOutputType struct {
 	ID   uint64 `sql:"AUTO_INCREMENT; NOT NULL"; json:"id"`
 	Name string `sql:"UNIQUE; NOT NULL"; json:"name"`
 	Unit string `sql:"UNIQUE; NOT NULL"; json:"unit"`
