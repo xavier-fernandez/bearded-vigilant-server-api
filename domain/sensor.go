2bb543d73d2d481284b6c50e6df634718ee6650f
Xavier Fernández Salas <xavier.fernandez.salas@gmail.com>
2015-08-25 10:50:31 +0200

    ENV-67: Add many to many between sensor and sensor_type

--- domain/sensor.go
+++ domain/sensor.go
@@ -1,7 +1,8 @@
 package domain
 
 type Sensor struct {
-	ID         uint64 `sql:"AUTO_INCREMENT; NOT NULL"; json:"id"`
-	PinNumber  uint32 `sql:"NOT NULL"; json:"pinNumber"`
-	GrovePiRow uint8  `sql:"NOT NULL"; json:"grovePiRow"`
+	ID          uint64       `sql:"AUTO_INCREMENT; NOT NULL"; json:"id"`
+	PinNumber   uint32       `sql:"NOT NULL"; json:"pinNumber"`
+	GrovePiRow  uint8        `sql:"NOT NULL"; json:"grovePiRow"`
+	SensorTypes []SensorType `gorm:"many2many:sensor_type_sensor;"`
 }
