e331f11889e7ddcf39913c30330e18332a6f0630
Xavier Fernández Salas <xavier.fernandez.salas@gmail.com>
2015-09-13 13:01:45 +0200

    Improve output when executing 'install_dependencies.sh'
    * Example output:
    
    ---------------------------------------------------
    Downloading Environ data Gatherer dependencies...
    ---------------------------------------------------
    Downloading GORM persistance framework...
    github.com/jinzhu/gorm (download)
    github.com/lib/pq (download)
    github.com/qor/inflection (download)
    DONE
    ---------------------------------------------------
    Downloading the HWIO I/O library...
    github.com/mrmorphic/hwio (download)
    DONE
    ---------------------------------------------------
    All the libraries have been downloaded successfully.

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,3 +1,23 @@
 #!/usr/bin/env bash
-go get -u "github.com/jinzhu/gorm"
-go get -u "github.com/mrmorphic/hwio"
+function printSlashes {
+   echo "---------------------------------------------------"
+}
+function printDone {
+   echo "DONE"
+}
+printSlashes
+echo "Downloading Environ data Gatherer dependencies..."
+printSlashes
+
+# Downloads the GORM persistence Framework: https://github.com/jinzhu/gorm
+echo "Downloading GORM persistance framework..."
+go get -u -v "github.com/jinzhu/gorm"
+printDone
+printSlashes
+
+# Downloads the HWIO I/O library. https://github.com/mrmorphic/hwio
+echo "Downloading the HWIO I/O library..."
+go get -u -v "github.com/mrmorphic/hwio"
+printDone
+printSlashes
+echo "All the libraries have been downloaded successfully."
